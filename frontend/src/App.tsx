0c958b16b70b2418ab6b27d124c1992d8a2e0d27
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-07-17 10:48:38 +0900

    chore: styled 설치

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
-export default function App() {
+function EmotionReact() {
   return (
     <div
       css={css`
@@ -12,6 +13,29 @@ export default function App() {
           color: red;
         }
       `}
-    ></div>
+    >
+      안녕 나는 Emotion React야
+    </div>
+  );
+}
+function EmotionStyled() {
+  const Wrapper = styled.div`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    &:hover {
+      color: red;
+    }
+  `;
+  return <Wrapper></Wrapper>;
+}
+
+export default function App() {
+  return (
+    <>
+      <EmotionReact />
+      <EmotionStyled />
+    </>
   );
 }
