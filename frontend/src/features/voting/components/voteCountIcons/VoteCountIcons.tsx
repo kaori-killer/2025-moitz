0ea426164e65ff8d85d022e9a4ae087f59097b2b
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-10-23 12:43:12 +0900

    feat: 투표 유저 아이콘은 투표 수와 상관없이 최대 5개가 보이도록 구현

--- frontend/src/features/voting/components/voteCountIcons/VoteCountIcons.tsx
+++ frontend/src/features/voting/components/voteCountIcons/VoteCountIcons.tsx
@@ -4,6 +4,8 @@ import IconVoteUser from '@icons/icon-vote-user.svg';
 
 import * as voteCountIcons from './voteCountIcons.styled';
 
+const MAX_VOTE_USER_ICON_COUNT = 5;
+
 interface VoteCountIconsProps {
   count: number;
 }
@@ -11,16 +13,19 @@ interface VoteCountIconsProps {
 function VoteCountIcons({ count }: VoteCountIconsProps) {
   return (
     <div css={[flex({ align: 'center' }), voteCountIcons.voteCountIcons()]}>
-      {Array.from({ length: count }, (_, index) => (
-        <img
-          key={index}
-          src={IconVoteUser}
-          alt="투표 유저 아이콘"
-          css={voteCountIcons.voteCountIcon({
-            zIndex: count - index,
-          })}
-        />
-      ))}
+      {Array.from(
+        { length: Math.min(count, MAX_VOTE_USER_ICON_COUNT) },
+        (_, index) => (
+          <img
+            key={index}
+            src={IconVoteUser}
+            alt="투표 유저 아이콘"
+            css={voteCountIcons.voteCountIcon({
+              zIndex: count - index,
+            })}
+          />
+        ),
+      )}
     </div>
   );
 }
