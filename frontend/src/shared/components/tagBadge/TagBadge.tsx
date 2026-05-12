49b8c845c00fb59f4736924d980a1066af3093f8
oa <eunsoa0310@gmail.com>
2026-05-12 15:44:49 +0900

    feat: TagBadge 컴포넌트 추가

---
+++ frontend/src/shared/components/tagBadge/TagBadge.tsx
@@ -0,0 +1,27 @@
+import { flex } from '@shared/styles/default.styled';
+
+import * as tagBadge from './tagBadge.styled';
+
+export type TagBadgeType = 'FAIRNESS' | 'MAX_BURDEN_RELIEF' | 'EFFICIENCY' | 'TRANSFER' | 'GENERAL';
+
+export const TAG_BADGE_LABEL: Record<TagBadgeType, string> = {
+  FAIRNESS: '가장 공평',
+  MAX_BURDEN_RELIEF: '최대 짧은',
+  EFFICIENCY: '최소 평균',
+  TRANSFER: '최소 환승',
+  GENERAL: '적당한',
+};
+
+interface TagBadgeProps {
+  type: TagBadgeType;
+}
+
+function TagBadge({ type }: TagBadgeProps) {
+  return (
+    <div css={[flex({ justify: 'center', align: 'center' }), tagBadge.base(type)]}>
+      <span css={tagBadge.text()}>{TAG_BADGE_LABEL[type]}</span>
+    </div>
+  );
+}
+
+export default TagBadge;
